4f33cc016b6fd43620d188915734ed29bbf4fb83
Jesusbaby <olayemifolakemi@yahoo.com>
2016-11-24 12:15:44 +0100

    update minMax tests and solutions

--- tests/minMaxTests.js
+++ tests/minMaxTests.js
@@ -6,6 +6,10 @@
 
  'use strict';
 
+var chai = require('chai')
+var expect = chai.expect;
+
+var myApp = require('../app/minMax.js')
 
 
  describe('Min-Max Numbers in a List: ', function () {
@@ -18,7 +22,7 @@
 
      it('should return [1,4] for [1, 2, 3 , 4]', function () {
 
-       expect(findMinMax([1, 2, 3, 4])).to.equal([1, 4]);
+       expect(myApp.findMinMax([1, 2, 3, 4])).to.equal([1, 4]);
 
      });
 
@@ -26,7 +30,7 @@
 
      it('should return [4, 6] for [6, 4]', function () {
 
-       expect(findMinMax([6, 4])).to.equal([4, 6]);
+       expect(myApp.findMinMax([6, 4])).to.equal([4, 6]);
 
      });
 
@@ -34,7 +38,7 @@
 
      it('should return [2, 78] for [4, 66, 6, 44, 7, 78, 8, 68, 2]', function () {
 
-       expect(findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).to.equal([2, 78]);
+       expect(myApp.findMinMax([4, 66, 6, 44, 7, 78, 8, 68, 2])).to.equal([2, 78]);
 
      });
 
@@ -50,7 +54,7 @@
 
      it('should return [4] for [4, 4, 4, 4]', function () {
 
-       expect(findMinMax([4, 4, 4, 4])).to.equal([4]);
+       expect(myApp.findMinMax([4, 4, 4, 4])).to.equal([4]);
 
      });
 
